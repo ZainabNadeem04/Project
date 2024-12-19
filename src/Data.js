0c416794a70a6e89bd0d6929ac2c108251258b16
ZainabNadeem04 <158503168+ZainabNadeem04@users.noreply.github.com>
2024-12-19 12:16:35 +0500

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,63 +1,55 @@
-export const Data = [
-        {
-            id: 1,
-            Title: "Fajita Large",
-            price: 800
-        },
-        {
-            id: 2,
-            Title: "Fajita Medium",
-            price: 600
-        },
-        {
-            id: 3,
-            Title: "Fajita Small",
-            price: 400
-        },
-        {
-            id: 4,
-            Title: "Pepperoni Large",
-            price: 900
-        },
-        {
-            id: 5,
-            Title: "Pepperoni Medium",
-            price: 700
-        },
-        {
-            id: 6,
-            Title: "Pepperoni Small",
-            price: 500
-        },
-        {
-            id: 7,
-            Title: "Margherita Large",
-            price: 850
-        },
-        {
-            id: 8,
-            Title: "Margherita Medium",
-            price: 650
-        },
-        {
-            id: 9,
-            Title: "Margherita Small",
-            price: 450
-        },
-        {
-            id: 10,
-            Title: "BBQ Chicken Large",
-            price: 950
-        },
-        {
-            id: 11,
-            Title: "BBQ Chicken Medium",
-            price: 750
-        },
-        {
-            id: 12,
-            Title: "BBQ Chicken Small",
-            price: 550
-        }
-    ];
-    
+export const Data = {
+    menu: {
+      pizza: [
+        { name: "Chicken Tikka", small: 400, medium: 850, large: 1100 },
+        { name: "Chicken Fajita", small: 400, medium: 850, large: 1100 },
+        { name: "Cheese Lover", small: 350, medium: 750, large: 950 },
+        { name: "Vege. Lover", small: 350, medium: 750, large: 950 },
+      ],
+      bar_b_q: [
+        { name: "Behari Kabab", small: 500, medium: 950, large: 1300 },
+        { name: "Kabab Crust", small: 500, medium: 950, large: 1300 },
+        { name: "Cheese Crust", small: 500, medium: 950, large: 1300 },
+        { name: "Emerald Pizza", small: 500, medium: 950, large: 1300 },
+      ],
+      beverages: [
+        { name: "Regular Drink", price: 80 },
+        { name: "Half Liter", price: 120 },
+        { name: "1 Liter", price: 170 },
+        { name: "1.5 Liter", price: 220 },
+      ],
+      wrap: [
+        { name: "Chicken Wrap", price: 250 },
+        { name: "Arabic Wrap", price: 270 },
+        { name: "Afghani Wrap", price: 280 },
+      ],
+      pasta: [
+        { name: "Chicken Cheese Pasta", half: 380, full: 480, category: "pasta" },
+      ],
+      nuggets: [
+        { name: "Chicken Nuggets", six: 250, twelve: 450, category: "nuggets" },
+        { name: "Oven Baked Wings", six: 440, twelve: 650, category: "nuggets" },
+      ],
+      
+      fries: [
+        { name: "Plain Fries", price: 250 },
+        { name: "Masala Fries", price: 270 },
+        { name: "Loaded Fries", price: 400 },
+      ],
+      dip_sauce: [
+        { name: "Bar. B.Q", price: 70 },
+        { name: "Garlic", price: 70 },
+        { name: "Hot Sauce", price: 70 },
+      ],
+      extras: [
+        { name: "Extra Topping", small: 50, medium: 70, large: 100 },
+      ],
+      
+    },
+  
+    special_offer: {
+      name: "Student Pizza",
+      price: 280,
+    },
+  };
+  
